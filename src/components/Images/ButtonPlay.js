5effc448d141589718a7de3c581f03bf25298d25
Simone Torrisi <storrisi@gmail.com>
2020-08-04 23:03:54 +0200

    hero section style and layout

---
+++ src/components/Images/ButtonPlay.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+
+const Image = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "button-play.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1920) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <Img
+      fluid={data.placeholderImage.childImageSharp.fluid}
+      className="button-play"
+    />
+  );
+};
+
+export default Image;
---
+++ src/components/Images/HeroImage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+
+const Image = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "Phone-Mockup.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1920) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <Img
+      fluid={data.placeholderImage.childImageSharp.fluid}
+      className="heroImage"
+    />
+  );
+};
+
+export default Image;
